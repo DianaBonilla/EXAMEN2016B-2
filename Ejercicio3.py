f32e15e0463e09183d87f62ecc97fc6b64871d29
USUARIO06-PC\Usuario01 <david-hernandezbgu@hotmail.com>
2016-12-07 11:57:06 -0500

    calculo volumen eliptico de la piscina

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -3,9 +3,18 @@ print("1. Piscina rectangular")
 print("2. Picina elíptica")
 print("3. Piscina circula")
 opcion = int(input("Ingrese la opcion que desea: "))
+
 if opcion == 1:
     a = float(input("Ingrese el valor del primer lado: "))
     b = float(input("Ingrese el valor del segundo lado: "))
     h = float(input("Ingrese el valor de la altura: "))
     volumen = a * b * h
     print("El volumen de la piscina rectangular es: ",volumen,"cm3")
+elif opcion == 2:
+    pi=3.1416
+    a=float(input("Ingrese el valor del largo del rectangulo: "))
+    b=float(input("Ingrese el valor del ancho del rectangulo: "))
+    c=float(input("Ingrese la altura del rectangulo: "))
+    area=pi*4*b
+    volumen=(((pi*a)/(2*b))/2)*c
+    print("El volumen de la piscina eliptica es: ",volumen)
