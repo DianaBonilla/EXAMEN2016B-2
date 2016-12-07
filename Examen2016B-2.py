dfbdff5a5635fee26b589e9b391985fbaf18f91c
USUARIO13-PC\Usuario01 <jordan_jssh_@hotmail.com>
2016-12-07 12:35:11 -0500

    Coloque el nombre del archivo del ejercicio 1 a las lineas de codigo del ejercicio 2

--- Examen2016B-2.py
+++ Examen2016B-2.py
@@ -11,7 +11,7 @@ def leertxt_ejercicio2():
     tiempo=0
     repeticion=0
     nombre='Jordan'
-    archi=open('****','r')
+    archi=open('nombres.txt','r')
     inicio=time()
     linea=archi.readline()
     cadena=linea.split(' ')
@@ -43,32 +43,8 @@ def ejercicio2():
     leertxt_ejercicio2()
 
 def ejercicio3():
-    print("Calculo del volumen de una piscina")
-    print("1. Piscina rectangular")
-    print("2. Picina elíptica")
-    print("3. Piscina circula")
-    opcion = int(input("Ingrese la opcion que desea: "))
-    pi=3.1416
-    if opcion == 1:
-        a = float(input("Ingrese el valor del primer lado: "))
-        b = float(input("Ingrese el valor del segundo lado: "))
-        h = float(input("Ingrese el valor de la altura: "))
-        volumen = a * b * h
-        print("El volumen de la piscina rectangular es: ",volumen,"cm3")
-    elif opcion == 2:
-        a=float(input("Ingrese el valor semieje MAYOR: "))
-        b=float(input("Ingrese el valor del semieje MENOR: "))
-        c=float(input("Ingrese la altura del rectangulo: "))
-        area=pi*a*b
-        volumen=area*c
-        print("El volumen de la piscina eliptica es: ",round(volumen,2),"cm3")
-    elif opcion == 3:
-        r = float(input("Ingrese el valor del radio del circulo: "))
-        area = pi * r**2
-        h = float(input("Ingrese la altura del rectangulo: "))
-        volumen = area * h
-        print("El volumen de la piscina circular es: ",round(volumen,3),"cm3")
-
+    ##Agregar ejercicio 3..!!
+    print("")
 
 print("\tExamen Bimestral")
 print("Bonilla Diana")
