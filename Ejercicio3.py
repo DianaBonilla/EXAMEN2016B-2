35ba2693b380883c7395a0877d5abfce63304099
USUARIO06-PC\Usuario01 <david-hernandezbgu@hotmail.com>
2016-12-07 12:12:37 -0500

    conclusion ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,28 +1,27 @@
-print("Calculo del volumen de una piscina")
-print("1. Piscina rectangular")
-print("2. Picina elíptica")
-print("3. Piscina circula")
-opcion = int(input("Ingrese la opcion que desea: "))
-pi=3.1416
-if opcion == 1:
-    a = float(input("Ingrese el valor del primer lado: "))
-    b = float(input("Ingrese el valor del segundo lado: "))
-    h = float(input("Ingrese el valor de la altura: "))
-    volumen = a * b * h
-    print("El volumen de la piscina rectangular es: ",volumen,"cm3")
-
-elif opcion == 2:
-    
-    a=float(input("Ingrese el valor del largo del rectangulo: "))
-    b=float(input("Ingrese el valor del ancho del rectangulo: "))
-    c=float(input("Ingrese la altura del rectangulo: "))
-    area=pi*4*b
-    volumen=(((pi*a)/(2*b))/2)*c
-    print("El volumen de la piscina eliptica es: ",volumen)
-
-elif opcion == 3:
-    r = float(input("Ingrese el valor del radio del circulo: "))
-    area = pi * r**2
-    h = float(input("Ingrese la altura del rectangulo: "))
-    volumen = area * h
-    print("El volumen de la piscina circular es: ",round(volumen,3))
+def piscina():
+    print("Calculo del volumen de una piscina")
+    print("1. Piscina rectangular")
+    print("2. Picina elíptica")
+    print("3. Piscina circula")
+    opcion = int(input("Ingrese la opcion que desea: "))
+    pi=3.1416
+    if opcion == 1:
+        a = float(input("Ingrese el valor del primer lado: "))
+        b = float(input("Ingrese el valor del segundo lado: "))
+        h = float(input("Ingrese el valor de la altura: "))
+        volumen = a * b * h
+        print("El volumen de la piscina rectangular es: ",volumen,"cm3")
+    elif opcion == 2:
+        a=float(input("Ingrese el valor semieje MAYOR: "))
+        b=float(input("Ingrese el valor del semieje MENOR: "))
+        c=float(input("Ingrese la altura del rectangulo: "))
+        area=pi*a*b
+        volumen=area*c
+        print("El volumen de la piscina eliptica es: ",round(volumen,2),"cm3")
+    elif opcion == 3:
+        r = float(input("Ingrese el valor del radio del circulo: "))
+        area = pi * r**2
+        h = float(input("Ingrese la altura del rectangulo: "))
+        volumen = area * h
+        print("El volumen de la piscina circular es: ",round(volumen,3),"cm3")
+piscina()
